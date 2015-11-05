0b4d867092e6b06f3363eca063a150003a1e2981
Miles Blackwood <milesblackwoodmusic@gmail.com>
2015-11-04 19:33:33 -0500

    Clean up

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
+// connect().use(serveStatic(__dirname)).listen(8080);
+
+var http = require('http'),
+		fs = require('fs');
+
+http.createServer(function(request, response) {
+
+	fs.readFile('./index.html', function (err, html) {
+	  if (err) {
+	  	response.writeHeader(404);
+	  	response.write('Nothing was found!');
+	  	response.end();
+	  } else {
+	  	response.writeHeader(200, {'Content-Type': 'text/html'});  
+	  	response.write(html);  
+	  	response.end();  
+		}
+	});
+
+}).listen(29600);
